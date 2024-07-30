93382cd4cb272fefd9774c33793cb10d649a8d58
Aziz Hmidi <AzizHmid10000@gmail.com>
2024-07-30 09:40:25 +0100

    dessert button commit

--- src/app/components/dessert/dessert.component.ts
+++ src/app/components/dessert/dessert.component.ts
@@ -1,21 +1,50 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Dessert } from '../../models/dessert.model';
 import { FormatPrice } from '../../utilities/FormatPrice';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dessert',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dessert.component.html',
   styleUrl: './dessert.component.scss',
 })
 export class DessertComponent implements OnInit {
   @Input() model!: Dessert;
   formattedPrice: string | null = null;
+  chosen: boolean = false;
+  nbItem: number = 0;
 
   constructor() {}
 
   ngOnInit() {
     this.formattedPrice = FormatPrice(this.model.price);
+  }
+
+  PickItem() {
+    if (this.chosen === false && this.nbItem === 0) {
+      this.chosen = true;
+      this.nbItem++;
+      return;
+    }
+    if (this.nbItem === 0) {
+      this.chosen = false;
+      return;
+    }
+  }
+
+  Increase() {
+    this.nbItem++;
+    if (this.nbItem === 0) {
+      this.PickItem();
+    }
+  }
+
+  Decrease() {
+    this.nbItem--;
+    if (this.nbItem === 0) {
+      this.PickItem();
+    }
   }
 }
